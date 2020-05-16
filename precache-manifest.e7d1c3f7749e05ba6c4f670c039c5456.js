2cdf749ee75a4cb999ab888f2d607de262d75e6c
Oliver Fleckenstein <oliver@fridayhome.net>
2020-05-16 22:02:45 +0200

    Updates

---
+++ precache-manifest.e7d1c3f7749e05ba6c4f670c039c5456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5314b6767bc47e050e37a0b3214d8b5d",
+    "url": "/palletten/index.html"
+  },
+  {
+    "revision": "1badd0c8e2ef745df7ec",
+    "url": "/palletten/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ccb4fdccb01c19b9fcdb",
+    "url": "/palletten/static/js/2.90801d44.chunk.js"
+  },
+  {
+    "revision": "21c75a98ff1b0cc08c27fd17e11b8c68",
+    "url": "/palletten/static/js/2.90801d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1badd0c8e2ef745df7ec",
+    "url": "/palletten/static/js/main.74fd5d71.chunk.js"
+  },
+  {
+    "revision": "4101d117babc1d945ca2",
+    "url": "/palletten/static/js/runtime-main.1cab7e8e.js"
+  }
+]);
