f9872d59df1d30a3dd441ca87e641c9b46eb3927
Oliver Fleckenstein <oliverfl@live.dk>
2021-01-03 17:02:08 +0100

    Writing the current state in text

--- src/Shelly.tsx
+++ src/Shelly.tsx
@@ -2,7 +2,7 @@ import React, { useCallback } from 'react';
 import { IoMdPower } from 'react-icons/io';
 import { setShellyState, stateToColor } from './App';
 import { Button } from './Button';
-import { IShelly } from './models';
+import { IShelly, State } from './models';
 
 export const Shelly = ({ shelly }: { shelly: IShelly }) => {
 	const toggle = useCallback(() => {
@@ -10,11 +10,17 @@ export const Shelly = ({ shelly }: { shelly: IShelly }) => {
 	}, [shelly]);
 
 	return (
-		<Button onClick={toggle} color={stateToColor(shelly.state)}>
-			<span className="inline-flex flex-col h-full justify-center px-2">
+		<Button
+			className="space-x-4"
+			onClick={toggle}
+			color={stateToColor(shelly.state)}
+		>
+			<span className="inline-flex flex-col h-full justify-center align-middle">
 				<IoMdPower size="20px" />
 			</span>
-			<span>{shelly.name}</span>
+			<span>
+				{shelly.name} - {shelly.state === State.On ? 'Tændt' : 'Slukket'}
+			</span>
 		</Button>
 	);
 };
